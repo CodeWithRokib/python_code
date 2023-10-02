8d7ddab01cb2daba7da9e51ce0a3170a867203e2
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2023-10-02 18:58:46 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+
+
+
+print( "Started python..")
+
+
+
+
+
+
+
+
+
+
---
+++ variable.py
@@ -0,0 +1,52 @@
+# variable started
+# x= 5
+# y = 'rokib'
+# print(x)
+# print(y)
+
+#how string, int, float varibale worked
+# x = str(3)
+# y = int(3)
+# z = float(3)
+# print(x,y,z)
+
+#how varibale type
+# x= 5
+# y = 'rokib'
+# print(type(x))
+# print(type(y))
+
+#varibale like a array
+# fruits = ["apple", "banana", "cherry"]
+# x, y, z = fruits
+# print(x)
+# print(y)
+# print(z)
+
+#variable connected if variable type not match this will be not worked
+# x = "Python "
+# y = "is "
+# z = "awesome"
+# print(x + y + z)
+
+#this is global variable
+# x = "awesome"
+#
+# def myfunc():
+#   x = "fantastic"
+#   print("Python is " + x)
+#
+# myfunc()
+#
+# print("Python is " + x)
+
+#if use global in before of varibale it will worked globally..
+# x = "awesome"
+#
+# def myfunc():
+#   global x
+#   x = "fantastic"
+#
+# myfunc()
+#
+# print("Python is " + x)
