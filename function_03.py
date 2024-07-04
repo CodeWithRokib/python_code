255851d42bf5e32bd1b7eb120aae03e209d0acdf
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2024-07-05 03:48:15 +0600

    function learning

---
+++ function_03.py
@@ -0,0 +1,3 @@
+def my_function(fname,age):
+    print(fname+" is " +age)
+my_function('Rokib','24')
---
+++ function_logic.py
@@ -0,0 +1,7 @@
+def my_function(age):
+    if age>18:
+        print('Adult')
+    else:
+        print('Not Adult')
+
+my_function(17)
